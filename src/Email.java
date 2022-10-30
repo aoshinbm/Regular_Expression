3dece59d569ef0e6f94fef6cfeedaf7dba2627f8
aoshinbm <aoshinbm28797@gmail.com>
2022-10-31 01:59:23 +0530

    Added: Validation of Email-id

---
+++ src/Email.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class Email {
+    static boolean isValid(String regex, String userInput)
+    {
+        return Pattern.compile(regex).matcher(userInput).matches();
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("User Registration ");
+        //[a-zA-Z]+([_+-.][a-zA-Z])*[@][a-zA-Z]+[.][a-z]{2,3}([.][a-zA-Z]{2})*$
+        //String emailPattern = "^[a-zA-Z]+[@][a-zA-Z]+[.][a-z]{2,3}([.][a-zA-Z]{2})*$";
+        String emailPattern = "^[a-zA-Z0-9#%&*_-]+[@][a-zA-Z]+[.][a-z]{2,4}([.][a-zA-Z]{2})*$";
+        System.out.println("Enter email-id:");
+        String email_id = sc.next();
+        System.out.println("Email_Id :"+email_id);
+        System.out.println(isValid(emailPattern, email_id));
+
+    }
+}
