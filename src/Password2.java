6db22a521128f4c802f5438403ca967cf3e32531
aoshinbm <aoshinbm28797@gmail.com>
2022-10-31 18:00:53 +0530

    Added: Validation of password with rule no.2 is added

---
+++ src/Password2.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class Password2 {
+    static boolean isValid(String regex, String userInput)
+    {
+        return Pattern.compile(regex).matcher(userInput).matches();
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("User Registration ");
+        String passwordPattern = "^[A-Z]{1}[A-Za-z0-9]{8,20}$";
+        System.out.println("Enter password:");
+        String password = sc.next();
+        System.out.println("Password :"+password);
+        System.out.println(isValid(passwordPattern,password));
+
+    }
+}
