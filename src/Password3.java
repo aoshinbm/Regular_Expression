be54ccf8e0466672810a9bdae924e08a69a36332
aoshinbm <aoshinbm28797@gmail.com>
2022-10-31 18:05:19 +0530

    Added: Validation of password with rule no.3 is added

---
+++ src/Password3.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class Password3 {
+    static boolean isValid(String regex, String userInput)
+    {
+        return Pattern.compile(regex).matcher(userInput).matches();
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("User Registration ");
+        String passwordPattern = "^[A-Z]{1}[0-9A-Za-z]{8,20}$";
+        System.out.println("Enter password:");
+        String password = sc.next();
+        System.out.println("Password :"+password);
+        System.out.println(isValid(passwordPattern,password));
+
+    }
+}
